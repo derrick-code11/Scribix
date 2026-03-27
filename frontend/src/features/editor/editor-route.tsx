43bfcb93438bde2278a63fc5154716b94599152f
derrick-code10 <demoemail1428@gmail.com>
2026-03-27 04:12:56 -0400

    Remove editor route wrapper

--- frontend/src/features/editor/editor-route.tsx
+++
@@ -1,7 +0,0 @@
-import { useParams } from 'react-router-dom'
-import { EditorPage } from '@/features/editor/editor-page'
-
-export function EditorRoute() {
-  const { postId } = useParams()
-  return <EditorPage key={postId} />
-}
