1468c3cae0522e6cff39154284197165c9277007
derrick-code10 <demoemail1428@gmail.com>
2026-03-27 04:12:54 -0400

    Remove tag.routes
    Removes tag CRUD routes that accompanied the private editor.

--- backend/src/routes/tag.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { searchTagsHandler } from "../controllers/tag.controller.js";
-import { authenticate } from "../middlewares/auth.js";
-
-const router = Router();
-
-router.get("/", authenticate, searchTagsHandler);
-
-export default router;
